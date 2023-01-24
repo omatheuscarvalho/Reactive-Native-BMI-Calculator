4e2a5290a1117d2c444253b9361091819a39f381
Matheus <matheusc200@gmail.com>
2023-01-24 18:37:57 -0300

    First

---
+++ src/components/ResultImc/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { View, Text } from "react-native"
+export default function ResultImc(props) {
+    return(
+        <View>
+            <Text>{props.messageResultImc}</Text>
+            <Text>{props.resultImc}</Text>
+        </View>
+    );
+}
