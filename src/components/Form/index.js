6a0aef5dc93d494f33b6c441c464581551c2951c
Matheus <matheusc200@gmail.com>
2023-01-26 18:35:55 -0300

    appfuncional

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,16 +1,32 @@
 import React, { useState } from "react"
-import { View, Text, TextInput, Button} from "react-native"
+import { View, Text, TextInput, TouchableOpacity} from "react-native"
 import ResultImc from "../ResultImc";
+import styles from './style'
+
 export default function Form() {
 
 const[height, setHeight] = useState(null)
 const[weight, setWeight] = useState(null)
-const[messageImc, setMessageImc] = useState("Preencha o peso e altura")
+const[messageImc, setMessageImc] = useState("fill high and weight")
 const[imc, setImc] = useState(null)
-const[textButton, setTextButton] = useState(null)
+const[textButton, setTextButton] = useState("Calculate")
+const[errorMessageHeigh, setErrorMessageHeigh] = useState(null)
+const[errorMessageWeight, setErrorMessageWeight] = useState(null)
 
 function imcCalculator(){
     return setImc((weight/(height*height)).toFixed(2))
+}
+function verifyImcIsNull(){
+    if(height == null){
+        setErrorMessageHeigh("Required field!")
+    }else{
+        setErrorMessageHeigh(null)
+    }
+    if(weight == null){
+        setErrorMessageWeight("Required field!")
+    }else{
+        setErrorMessageWeight(null)
+    }
 }
 function validationImc(){
     if(weight != null && height != null){
@@ -19,34 +35,44 @@ function validationImc(){
         setWeight(null)
         setMessageImc("Your Imc is equal to")
         setTextButton("Calculate again")
+        setErrorMessageWeight(null)
+        setErrorMessageHeigh(null)
         return
     }
+    verifyImcIsNull()
     setImc(null)
     setTextButton("Calculate")
     setMessageImc("Fill weight and heigh")
 }
     return(
-        <View>
-            <View>
-                <Text>Hight</Text>
+        <View style={styles.formContent}>
+            <View style={styles.form}>
+                <Text style={styles.formLabel}>Hight</Text>
+                <Text style={styles.errorMessage}>{errorMessageHeigh}</Text>
                 <TextInput
+                style={styles.imput}
                 onChangeText={setHeight}
                 value={height}
                 placeholder="Ex: 1.75"
                 keyboardType="numeric"
                 />
 
-                <Text>Weight</Text>
+                <Text style={styles.formLabel}>Weight</Text>
+                <Text style={styles.errorMessage}>{errorMessageWeight}</Text>
                 <TextInput
+                style={styles.imput}
                 onChangeText={setWeight}
                 value={weight}
                 placeholder="Ex: 70.25"
                 keyboardType="numeric"
                 />
-                <Button
-                onPress={() => validationImc()}
-                title="Calculate IMC"
-                />
+                <TouchableOpacity
+                style={styles.ButtonCalculator}
+                onPress={()=>{
+                    validationImc()
+                }}>
+                    <Text style={styles.textButtonCalculator}>{textButton}</Text>
+                </TouchableOpacity>
             </View>
             
             <ResultImc
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { View, Text} from "react-native"
-
+import styles from './style'
 export default function Title() {
     return(
-        <View>
-            <Text>HEALTH APP</Text>
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>HEALTH APP</Text>
         </View>
     );
 }
